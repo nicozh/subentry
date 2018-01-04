13307572777f9d856689d08c60a5b37f5cd8572c
zhang <mitmiky@outlook.com>
2018-01-04 23:36:08 +0800

    apple

---
+++ carousel-3/main.js
@@ -0,0 +1,42 @@
+
+var children=$('.tags>li')
+for(let i=0;i<children.length;i++){
+  $(children[i]).on('click',function(y){
+    var i = $(y.currentTarget).index()
+    var p = i*-800
+    $('.images').css({
+      transform: 'translate(' + p + 'px)'
+    })
+
+  n=i  
+  children.eq(n).addClass('active')
+ .siblings('.active').removeClass('active')
+   
+      
+    
+  })
+}
+
+
+n=0
+
+ timerId=setInterval(()=>{
+ children.eq(n%6).trigger('click')
+ 
+  n+=1
+},1000)
+
+
+$('#window').on('mouseenter', function() {
+  window.clearInterval(timerId)
+})
+
+
+$('#window').on('mouseleave', function() {
+ 
+  timerId=setInterval(()=>{
+  children.eq(n%6).trigger('click')
+  n+=1
+},2000)
+  
+})
