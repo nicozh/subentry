568c32c012d74a9187541f65effce4e1c178aabe
zhang <mitmiky@outlook.com>
2018-01-07 23:24:18 +0800

    JSONP

---
+++ JSONP/server.js
@@ -0,0 +1,60 @@
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+var port = process.argv[2]
+
+if(!port){
+  console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
+  process.exit(1)
+}
+
+var server = http.createServer(function(request, response){
+  var parsedUrl = url.parse(request.url, true)
+  var path = request.url 
+  var query = ''
+  if(path.indexOf('?') >= 0){ query = path.substring(path.indexOf('?')) }
+  var pathNoQuery = parsedUrl.pathname
+  var queryObject = parsedUrl.query
+  var method = request.method
+
+  /******** 从这里开始看，上面不要看 ************/
+
+  console.log('HTTP 路径为\n' + path)
+  if(path == '/style.css'){
+    var string=fs.readFileSync('./style.css','utf8')
+    response.setHeader('Content-Type', 'text/css; charset=utf-8')
+    response.write(string)
+    response.end()
+  }else if(path == '/main.js'){
+    var string=fs.readFileSync('./main.js','utf8')
+    response.setHeader('Content-Type', 'application/javascript')
+    response.write(string)
+    response.end()
+  }else if(path == '/'){
+    var string=fs.readFileSync('./index.html','utf8')
+    response.setHeader('Content-Type', 'text/html; charset=utf-8')
+    response.write(string)
+    response.end()
+  }else if(path == '/pay'){
+    var amount =fs.readFileSync('./db','utf8')
+    var newAmount = amount - 1
+    fs.writeFileSync('./db',newAmount)
+    response.setHeader('Content-type','application/javascript')
+    response.statusCode = 200
+    response.write(`
+      123
+    `)
+    response.end()
+  }
+  else{
+    response.statusCode = 404
+    response.setHeader('Content-Type', 'text/html; charset=utf-8')
+    response.write()
+    response.end()
+  }
+
+  /******** 代码结束，下面不要看 ************/
+})
+
+server.listen(port)
+console.log('监听 ' + port + ' 成功\n请用浏览器打开 http://localhost:' + port)
