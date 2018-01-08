2cf257abc962181c0392e931dfd145afc2d4f8af
zhang <mitmiky@outlook.com>
2018-01-08 15:49:41 +0800

    重写

--- JSONP/server.js
+++ JSONP/server.js
@@ -1,60 +1,54 @@
 var http = require('http')
 var fs = require('fs')
 var url = require('url')
-var port = process.argv[2]
 
-if(!port){
-  console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
-  process.exit(1)
-}
+//console.log(Object.keys(http))
+var port = process.env.PORT || 8888;
 
 var server = http.createServer(function(request, response){
-  var parsedUrl = url.parse(request.url, true)
-  var path = request.url 
-  var query = ''
-  if(path.indexOf('?') >= 0){ query = path.substring(path.indexOf('?')) }
-  var pathNoQuery = parsedUrl.pathname
-  var queryObject = parsedUrl.query
-  var method = request.method
 
-  /******** 从这里开始看，上面不要看 ************/
+var temp = url.parse(request.url, true)
+var path = temp.pathname
+var query = temp.query
+var method = request.method
 
-  console.log('HTTP 路径为\n' + path)
-  if(path == '/style.css'){
-    var string=fs.readFileSync('./style.css','utf8')
-    response.setHeader('Content-Type', 'text/css; charset=utf-8')
-    response.write(string)
-    response.end()
-  }else if(path == '/main.js'){
-    var string=fs.readFileSync('./main.js','utf8')
-    response.setHeader('Content-Type', 'application/javascript')
-    response.write(string)
-    response.end()
-  }else if(path == '/'){
-    var string=fs.readFileSync('./index.html','utf8')
-    response.setHeader('Content-Type', 'text/html; charset=utf-8')
-    response.write(string)
-    response.end()
-  }else if(path == '/pay'){
-    var amount =fs.readFileSync('./db','utf8')
-    var newAmount = amount - 1
-    fs.writeFileSync('./db',newAmount)
-    response.setHeader('Content-type','application/javascript')
-    response.statusCode = 200
-    response.write(`
-      123
-    `)
-    response.end()
-  }
-  else{
-    response.statusCode = 404
-    response.setHeader('Content-Type', 'text/html; charset=utf-8')
-    response.write()
-    response.end()
-  }
+  //从这里开始看，上面不要看
 
-  /******** 代码结束，下面不要看 ************/
+if(path === '/'){  // 如果用户请求的是 / 路径
+  var string = fs.readFileSync('./index.html', 'utf8')
+  var amount=fs.readFileSync('./db','utf8')
+  string=string.replace('&amount&',amount)  
+  response.setHeader('Content-Type', 'text/html;charset=utf-8')  
+  response.write(string)
+  response.end()   
+}else if(path === '/style.css'){   
+  var string = fs.readFileSync('./style.css', 'utf8')
+  response.setHeader('Content-Type', 'text/css')
+  response.write(string)
+  response.end()
+}else if(path === '/main.js'){  
+  var string = fs.readFileSync('./main.js', 'utf8')
+  response.setHeader('Content-Type', 'application/javascript')
+  response.write(string)
+  response.end(string)
+}else if(path === '/pay'){
+  var amount =fs.readFileSync('./db','utf8')
+  var newAmount = amount - 1
+  fs.writeFileSync('./db',newAmount)
+  response.setHeader('Content-type','application/javascript')
+  response.write(`${query.callback}.call(undefined,'success')`)
+  response.statusCode = 200
+  response.end()
+}else{  
+  response.statusCode = 404
+  response.setHeader('Content-Type', 'text/html;charset=utf-8') 
+  response.write('找不到对应的路径，你需要自行修改 index.js')
+  response.end()
+}
+
+// 代码结束，下面不要看
+console.log(method + ' ' + request.url)
 })
 
 server.listen(port)
-console.log('监听 ' + port + ' 成功\n请用浏览器打开 http://localhost:' + port)
+console.log('监听 ' + port + ' 成功，请用浏览器打开 http://localhost:' + port)
